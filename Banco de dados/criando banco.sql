b7dd758b3995b53029f167895c1960534a811172
Felipecardosodacosta <felipe.cardoso.costa96@gmail.com>
2025-08-18 21:58:07 -0300

    ac

--- Banco de dados/criando banco.sql
+++ Banco de dados/criando banco.sql
@@ -25,3 +25,49 @@ REFERENCES fornecedor (id_fornecedor);
 ALTER TABLE peca
 DROP COLUMN viado;
 
+
+
+CREATE DATABASE instituicao_ensino;
+
+USE instituicao_ensino;
+
+CREATE TABLE sala (
+id_sala INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome_sala VARCHAR(45),
+capacidade_sala INT
+);
+
+CREATE TABLE turma(
+id_turma INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome_turma VARCHAR(45),
+periodo VARCHAR(45),
+sala_id INT,
+FOREIGN KEY(sala_id)
+REFERENCES sala (id_sala)
+);
+
+
+
+CREATE TABLE professor(
+id_professor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome_professor VARCHAR(45),
+formacao_academica VARCHAR(45),
+turma_id INT,
+FOREIGN KEY(turma_id)
+REFERENCES turma (id_turma)
+);
+
+
+
+CREATE TABLE aula(
+id_aula INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome_aula VARCHAR(45),
+professor_id INT,
+FOREIGN KEY (professor_id)
+REFERENCES professor (id_professor)
+);
+
+
+
+
+
