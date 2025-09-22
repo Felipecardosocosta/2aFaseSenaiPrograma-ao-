1e3be69b24196ca0414644809d05fc31d1fb667a
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-09-22 18:52:26 -0300

    tes

--- introreact/aula4/testando_api/src/components/Notification/NotificationText.jsx
+++ introreact/aula4/testando_api/src/components/Notification/NotificationText.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function NotificationText({content,imgTest=false}) {
   return (
     <>
-      <h3>{content}</h3>
       {imgTest && <img  src={imgTest} alt=""/>}
+      <h3>{content}</h3>
     </>
   )
 }
