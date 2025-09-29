7e1d17ea71d5aa1af969a6fcb3c7583d83703b6d
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-09-28 23:10:22 -0300

    t

--- introreact/aula7/paineldeoracoes/src/components/Pagina.jsx
+++ introreact/aula7/paineldeoracoes/src/components/Pagina.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Pagina.css'
 
 function Pagina({conteudo=[]}) {
   return (
-    <div>
+    <div className='divConteudo'>
       {conteudo.map(texto=> (<p>{texto}</p>))}
     </div>
   )
