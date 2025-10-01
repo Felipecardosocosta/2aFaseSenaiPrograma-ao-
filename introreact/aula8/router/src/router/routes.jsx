19e41780dafe395e1d9b7732718591605030ce4e
FELIPE CARDOSO DA COSTA <felipe_cardoso_costa@estudante.sc.senai.br>
2025-09-30 21:47:33 -0300

    adc

---
+++ introreact/aula8/router/src/router/routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home'
+import Produtos from '../pages/Produtos'
+import Cadastro from '../pages/Cadastro'
+
+
+const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/produtos', element: <Produtos /> },
+    { path: '/cadastro', element: <Cadastro /> }
+])
+
+export default router
+
