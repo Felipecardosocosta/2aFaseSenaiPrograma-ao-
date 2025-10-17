4a17011410e91bc654e3a617cd5f44914d334332
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-10-16 23:46:57 -0300

    con

--- introreact/aula9/atividades/src/pages/Home.jsx
+++ introreact/aula9/atividades/src/pages/Home.jsx
@@ -3,10 +3,19 @@ import NavBar from '../components/NavBar'
 
 
 function Home() {
+
+    fetch('http://localhost:3000/users',{
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({})
+    })
+
     return (
 
         <div>
             <NavBar />
+
+            
 
         </div>
     )
